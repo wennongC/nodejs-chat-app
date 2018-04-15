a873b1c1cee4d884a3e4fc3cfaf8d23536add194
wennerc <caiwennong@gmail.com>
2018-04-16 00:09:18 +1000

    Add connect and disconnect Message

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../public'); //join can omit unneccesry path
 const port = process.env.PORT || 3000;
-const app = express();
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("New user connected");
+
+  socket.on("disconnect", () => {
+    console.log("A User Disconnected");
+  });
+});
+
+server.listen(port, () => {
   console.log(`Started on the port ${port}`);
 });
