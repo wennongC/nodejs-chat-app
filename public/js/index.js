2dc82e41f7a2503f2ee8b54a4891731fe21da2a6
wennerc <caiwennong@gmail.com>
2018-05-21 23:29:43 +1000

    add style on the message block

--- public/js/index.js
+++ public/js/index.js
@@ -14,19 +14,26 @@ socket.on("currentPeople", function(num){
 
 socket.on("newMessage", function(message) {
   var formattedTime = moment(message.createAt).format('h:mm a');
-  var $li = $('<li></li>');
-  $li.text(`${message.from} (${formattedTime}): ${message.text}`);
-  $('#messages').append($li);
+  var template = $('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createAt: formattedTime
+  });
+
+  $('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message){
   var formattedTime = moment(message.createAt).format('h:mm a');
-  var $li = $('<li></li>');
-  var $a = $('<a target="_blank">My Current Location</a>');
-  $li.text(`${message.from} (${formattedTime}): `);
-  $a.attr('href', message.url);
-  $li.append($a);
-  $('#messages').append($li);
+  var template = $('#location-message-template').html();
+  var html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createAt: formattedTime
+  });
+
+  $('#messages').append(html);
 });
 
 $('#message-form').on("submit", function(e){
