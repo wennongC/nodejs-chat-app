54015bd3c4469b3e8c40f429f1f2b4828b6670e3
wennerc <caiwennong@gmail.com>
2018-05-24 23:18:51 +1000

    auto-scroll function

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom(){
+  //Selectors
+  var $messages = $('#messages');
+  var $newMessage = $messages.children('li:last-child');
+  //Height
+  var clientHeight = $messages.prop('clientHeight');
+  var scrollTop = $messages.prop('scrollTop');
+  var scrollHeight = $messages.prop('scrollHeight');
+  var newMessageHeight = $newMessage.innerHeight();
+  var lastMessageHeight = $newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    $messages.scrollTop(scrollHeight);
+  };
+}
+
 socket.on("connect", function() {
   console.log("connected to server");
 });
@@ -22,6 +38,7 @@ socket.on("newMessage", function(message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -34,6 +51,7 @@ socket.on('newLocationMessage', function(message){
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 $('#message-form').on("submit", function(e){
